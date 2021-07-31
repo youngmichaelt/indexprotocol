ce968a3954ec2714f1b1f36a599f16a00d89df76
Mac <mac@Macs-MacBook.local>
2021-07-31 15:42:22 -0400

    Added functionality for testing in remix with chainlink oracle.

--- contracts/remixExamples/ProtocolWOracle.sol
+++ contracts/remixExamples/ProtocolWOracle.sol
@@ -1,10 +1,13 @@
-pragma solidity ^0.8.6;
+pragma solidity ^0.6.7;
+import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
 
 import "./lib/SafeMathInt.sol";
 import "./lib/UInt256Lib.sol";
 
 
-contract IndexProtocol {
+
+
+contract IndexProtocol is ChainlinkClient{
     // using SafeMath for uint256;
     using SafeMathInt for int256;
     using UInt256Lib for uint256;
@@ -21,6 +24,15 @@ contract IndexProtocol {
     string public name = "IndexProtocol";
     string public symbol = "INDX";
     
+    //oracle stuff
+    uint256 public volume;
+    uint256 public oracleMarket;
+    uint256 public oracleTarget;
+    
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+    
     
     uint public totalShares;
     uint public sharesPer;
@@ -29,10 +41,19 @@ contract IndexProtocol {
     
     event Approval(address indexed owner, address indexed spender, uint value);
     
-    constructor(){
+    constructor() public {
         balances[msg.sender] = totalSupply;
         holders.push(msg.sender);
         index[msg.sender] = holders.length;
+        
+        //oracle stuff 
+        setPublicChainlinkToken();
+        oracle = 0x3A56aE4a2831C3d3514b5D7Af5578E45eBDb7a40;
+        jobId = "3b7ca0d48c7a4b2da9268456665d11ae";
+        fee = 0.1 * 10 ** 18; // 0.1 LINK
+        
+        
+        
         // totalShares = totalSupply;
         // sharesPer = totalShares/totalSupply;
         // shareBalances[msg.sender] = totalShares;
@@ -127,17 +148,23 @@ contract IndexProtocol {
     
     
     
-    function rebase(uint256 targetPrice, uint256 marketPrice) public returns (bool){
+    function rebase() public returns (bool){
         
         // uint256 change = 1;
         uint256 supplyDelta;
         uint256 userDelta;
-        // uint256 targetPrice = 10;
-        // uint256 marketPrice = 9;
+        uint256 targetPrice = 10;
+        uint256 marketPrice = 9;
         int256 change;
         bool neg = false;
         
-        change = deltaChange(targetPrice, marketPrice);
+        
+        uint timesAmount = 10**15;
+        oracleTarget = oracleTarget / timesAmount;
+        oracleMarket = oracleMarket / timesAmount;
+        
+        
+        change = deltaChange(oracleTarget, oracleMarket);
         
         if (change == 0){
             //No rebase needed
@@ -177,6 +204,60 @@ contract IndexProtocol {
             
         }
         return true;
+    }
+    
+    //oracle stuff
+    function startRebase() public returns (bytes32 requestId) 
+    {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        
+        
+        request.add("get", "https://api.pancakeswap.info/api/v2/tokens/0x09ad270120d873a2b2fab0b01dff00c679880919");
+        
+        
+        request.add("path", "data.price");
+        
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int timesAmount = 10**18;
+        request.addInt("times", timesAmount);
+        
+        // Sends the request
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+    function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId)
+    {
+        oracleTarget = _volume;
+        volume = _volume;
+        if (oracleTarget != 0){
+            oracleMarketPrice();
+        }
+        
+    }
+    function oracleMarketPrice() public returns (bytes32 requestId) 
+    {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfillMarket.selector);
+        
+        
+        request.add("get", "https://api.pancakeswap.info/api/v2/tokens/0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c");
+        
+        
+        request.add("path", "data.price");
+        
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int timesAmount = 10**18;
+        request.addInt("times", timesAmount);
+        
+        // Sends the request
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+    function fulfillMarket(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId)
+    {
+        oracleMarket = _volume;
+        volume = _volume;
+        if (oracleMarket != 0){
+            rebase();
+        }
+        
     }
     
     
